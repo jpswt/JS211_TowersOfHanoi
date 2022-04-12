7e7d3180755e01053410415d0b0a3fb1fe50d91e
jpswt <joel_swt@yahoo.com>
2022-04-12 18:10:43 -0500

    first commit all working

--- main.js
+++ main.js
@@ -2,25 +2,28 @@
 
 const assert = require('assert');
 const readline = require('readline');
+const { start } = require('repl');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
 // An object that represents the three stacks of Towers of Hanoi; 
-  // * each key is an array of Numbers: 
-    // * A is the far-left, 
-    // * B is the middle, 
-    // * C is the far-right stack
-      // * Each number represents the largest to smallest tokens: 
-        // * 4 is the largest, 
-        // * 1 is the smallest
+// * each key is an array of Numbers: 
+// * A is the far-left, 
+// * B is the middle, 
+// * C is the far-right stack
+// * Each number represents the largest to smallest tokens: 
+// * 4 is the largest, 
+// * 1 is the smallest
 
 let stacks = {
   a: [4, 3, 2, 1],
   b: [],
   c: []
 };
+
+let moveCount = 0;
 
 // Start here. What is this function doing?
 const printStacks = () => {
@@ -30,25 +33,71 @@ const printStacks = () => {
 }
 
 // Next, what do you think this function should do?
-const movePiece = () => {
+
+// Declaring a function movePiece with parameters of startStack and endStack
+// that will take the last value of the startStack array and push it to the 
+// end of the endStack array
+const movePiece = (startStack, endStack) => {
+  // variable that stores the last value of the starting stack array
+  let movingPiece = stacks[startStack].pop()
+  // pushing the the value of movingPiece to the end of the ending stack array
+  stacks[endStack].push(movingPiece)
   // Your code here
 
 }
 
 // Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
-const isLegal = () => {
+
+// Decalaring the function isLegal with parameters of startStack and endStack that determine if a move if legal
+const isLegal = (startStack, endStack) => {
+  // if statement that determines if the stack you are moving a piece to is empty. If it is empty, 
+  // it will return true
+  if (stacks[endStack].length === 0) {
+    return true
+    // Else if statement that determines if piece at the destination stack is bigger than the piece
+    // at the source stack.  If the destination piece is larger than the source piece being moved
+    // on top of it, it will return true.
+  } else
+    if (stacks[endStack].slice(-1) > stacks[startStack].slice(-1)) {
+      return true
+      // If niether of the two conditions are met, it will return false.
+    } else return false
   // Your code here
 
 }
 
 // What is a win in Towers of Hanoi? When should this function run?
+
+//Declaring a function that checks for a win
 const checkForWin = () => {
+  // If statement that determines if the either stack 'b' or stack 'c' have
+  // all four pieces.  If either stack is equal to four, it returns true, 
+  // else it will return false.
+  if (stacks['b'].length === 4 || stacks['c'].length === 4) {
+    return true
+  } else return false
+
   // Your code here
 
 }
 
 // When is this function called? What should it do with its argument?
+
+// Declaring a function towersOfHanoi with parameters startStack and endStack that starts the puzzle
 const towersOfHanoi = (startStack, endStack) => {
+  // if the isLegal function is true, it will invoke the movePiece function to move a piece from the 
+  // starting stack to the end stack
+  if (isLegal(startStack, endStack)) {
+    movePiece(startStack, endStack)
+    moveCount++
+    // It will then invoke the checkForWin function to see if all four pieces are on the stack
+    if (checkForWin()) {
+      console.log(`Congrats, you are a winner and did it ${moveCount} moves!! `)
+    }
+    //if isLegal function is false, it will display error message to try again
+  } else {
+    console.log('Move not valid. Please try again.')
+  }
   // Your code here
 
 }
